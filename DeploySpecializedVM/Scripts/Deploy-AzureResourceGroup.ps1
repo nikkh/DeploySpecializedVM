1130ec8080b4a45e2ac7f90e7643b9de428d4c75
nikkh <nikkh.noreply.github.com>
2016-04-29 15:11:30 +0100

    these are the scripts that I used to re-create tony's environment

---
+++ DeploySpecializedVM/Scripts/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,41 @@
+﻿#Requires -Version 3.0
+#Requires -Module AzureRM.Resources
+#Requires -Module Azure.Storage
+
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [string] $ResourceGroupName = 'ispoc3',
+    [switch] $UploadArtifacts,
+    [string] $StorageAccountName,
+    [string] $StorageAccountResourceGroupName, 
+    [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
+    [string] $TemplateFile = '..\Templates\deploySQLVM.json',
+    [string] $TemplateParametersFile = '..\Templates\deploySQLVM.parameters.json',
+    [string] $ArtifactStagingDirectory = '..\bin\Debug\staging',
+    [string] $AzCopyPath = '..\Tools\AzCopy.exe',
+    [string] $DSCSourceFolder = '..\DSC'
+)
+
+Import-Module Azure -ErrorAction SilentlyContinue
+
+try {
+    [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(" ","_"), "2.8")
+} catch { }
+
+Set-StrictMode -Version 3
+
+$OptionalParameters = New-Object -TypeName Hashtable
+$TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile)
+$TemplateParametersFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile)
+
+
+
+# Create or update the resource group using the specified template file and template parameters file
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop 
+
+New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                   -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -TemplateParameterFile $TemplateParametersFile `
+                                   @OptionalParameters `
+                                   -Force -Verbose
